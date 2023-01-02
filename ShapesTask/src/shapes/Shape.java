99240bb6395651be02d7c7401f07322ac9690ee9
Mohamed Ishak <mohamed19.damir@gmail.com>
2023-01-02 09:17:33 +0200

    First Commit

---
+++ ShapesTask/src/shapes/Shape.java
@@ -0,0 +1,13 @@
+package shapes;
+
+public abstract class Shape {
+
+  protected double area ;
+  protected double perimeter;
+
+    public abstract void calcArea();
+
+    public abstract void calcPerimeter();
+
+
+}
